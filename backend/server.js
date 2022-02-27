71627e1d0add29334a0a8f520f92e6434fee4e93
asd <asd@gmail.com>
2022-02-27 23:00:06 +0700

    dssdsd

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,40 @@
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-const csrf = require('csurf');
+const csrf = require("csurf");
 
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
-const path = require('path');
+const path = require("path");
 
 // const csrfProtection = csrf({ cookie: true });
 
 // const parseForm = bodyParser.urlencoded({ extended: false });
 
-app.use(cors());
+const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-require('./routes/admin/AdminRoute')(app);
-require('./routes/authen')(app);
-require('./routes/category')(app);
-require('./routes/favourite')(app);
-require('./routes/product')(app);
-require('./routes/cart')(app);
-require('./routes/review')(app);
-require('./routes/order')(app);
+require("./routes/admin/AdminRoute")(app);
+require("./routes/authen")(app);
+require("./routes/category")(app);
+require("./routes/favourite")(app);
+require("./routes/product")(app);
+require("./routes/cart")(app);
+require("./routes/review")(app);
+require("./routes/order")(app);
 
-app.listen(3000, function () {
-    console.log('http://localhost:3000');
-})
+app.listen(PORT, function () {
+  console.log("http://localhost:3000");
+});
