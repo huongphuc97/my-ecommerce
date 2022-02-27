83243b0ac6e7eec256a920ac1a33e7e2ed06f4c3
asd <asd@gmail.com>
2022-02-27 23:58:10 +0700

    jgfd

--- backend/connect.js
+++ backend/connect.js
@@ -1,15 +1,24 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'',
-    database: 'fullproject',
-    multipleStatements: true
+// const conn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password:'',
+//     database: 'fullproject',
+//     multipleStatements: true
+// });
+
+const conn = require("serverless-mysql")({
+  config: {
+    host: process.env.ENDPOINT,
+    database: process.env.DATABASE,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+  },
 });
-conn.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+conn.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
 module.exports = conn;
